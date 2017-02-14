b2b744842041a25f2ec5af65f03f9d56801981d0
Madhvesh Navkal <manavkal@visa.com>
2017-02-14 13:15:29 -0800

    updated to hash the message

--- config/settings.js
+++ config/settings.js
@@ -6,9 +6,9 @@
 var config = {};
 
 config.web3Location = 'http://localhost:8545';
-config.fromAccount = '';
-config.toAccount = '';
-config.dataDirectory = '';
-config.password = ''
+config.fromAccount = '0xdcf6fd30ba048336cb39f8f0fd06cd63aeef515c';
+config.toAccount = '0xdcf6fd30ba048336cb39f8f0fd06cd63aeef515c';
+config.dataDirectory = '/Users/manavkal/ethereum-local/temp';
+config.password = 'password123'
 
 module.exports = config;
--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var config = require('./config/settings');
 
 var web3 = new Web3(new Web3.providers.HttpProvider(config.web3Location));
 
-var message = 'VISA';
+//var message = 'VISA';
+var message = web3.sha3('VISA');
 
 if(web3.isConnected()) {
  sendRawTransaction(config.fromAccount,config.toAccount,message);
@@ -29,7 +30,6 @@ app.listen(app.get('port'), function() {
 
 
 
-
 function sendRawTransaction(fromAddress,toAddress,data) {
 
   var datadir = config.dataDirectory;
@@ -38,23 +38,24 @@ function sendRawTransaction(fromAddress,toAddress,data) {
 
   var from = fromAddress;
   var to =  toAddress;
-  var nonce =  web3.eth.getTransactionCount(from) ;
-  var nonceHex = web3.toHex(nonce);
-  var gasPrice = web3.eth.gasPrice;
 
-  //This is not implemented
-  //var gasLimit = web3.eth.gasLimit;
+  var nonce =  web3.eth.getTransactionCount(from) ;
+  var gasPrice = web3.eth.gasPrice;
   //Will get the gasLimit from latest transaction
   var transactionLatest = web3.eth.getBlock('latest');
   var gasLimit = transactionLatest.gasLimit;
 
 
-  //Get the price in hex for rawTx
+  //Get the price in hex for submitting i rawTx
   var gasPriceHex = web3.toHex(gasPrice);
   var gasLimitHex = web3.toHex(gasLimit);
-  var gas = 200000;
-  var value = 1;
+  var nonceHex = web3.toHex(nonce);
 
+  //Not transferring any ether so setting it to 0
+  var value = 0;
+
+  //Get the estimated gas for this transaction, here data is just hash..if this is a smart contract replace with solidity code
+  var gas = web3.eth.estimateGas({from:from,to:to,value:value,data: data});
 
   var rawTx = {
     nonce: nonceHex,
@@ -62,11 +63,8 @@ function sendRawTransaction(fromAddress,toAddress,data) {
     to: to,
     value: value,
     gas: gas,
-    //100000
     gasPrice: gasPriceHex,
-    //20000000000
     gasLimit:  gasLimitHex,
-    //4712388,100000
     data: data
   };
   var tx = new Tx(rawTx);
@@ -74,11 +72,9 @@ function sendRawTransaction(fromAddress,toAddress,data) {
 
   var serializedTx = tx.serialize();
 
-  console.log('From: -', from, 'To:- ', to, 'Data: ', data,'GasPrice: ',gasPriceHex,'Gas Limit',gasLimit);
-
   web3.eth.sendRawTransaction(serializedTx.toString('hex'), function(err, hash) {
     if (!err)
-      console.log(hash);
+      console.log('Transaction Hash: ',hash);
   });
 
 }
